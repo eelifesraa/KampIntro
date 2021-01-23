1aa0315f3d61f60ef7b7591111b5517afb9df5ab
Elif Esra Eker <ekerelifesra@gmail.com>
2021-01-23 23:51:49 +0300

    23.01.2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            //ihtiyacKrediManager.Hesapla();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            ILoggerService databaseloggerServise = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+            //*********************//
+
+            IKrediManager esnafkrediManager = new EsnafKredisiManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new DatabaseLoggerService());
+            basvuruManager.BasvuruYap(konutKrediManager,databaseloggerServise);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
---
+++ Oop2/Musteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    //base sınıf
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+    }
+}
